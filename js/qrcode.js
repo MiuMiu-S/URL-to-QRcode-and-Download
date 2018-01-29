aadf4cbe2ecbf0773dda3fd1f6a157000168745b
MiuMiu-S <3290555376@qq.com>
2018-01-29 18:06:13 +0800

    (*^▽^*)

--- js/qrcode.js
+++ js/qrcode.js
@@ -8,6 +8,7 @@
  * @see <a href="http://www.d-project.com/" target="_blank">http://www.d-project.com/</a>
  * @see <a href="http://jeromeetienne.github.com/jquery-qrcode/" target="_blank">http://jeromeetienne.github.com/jquery-qrcode/</a>
  */
+
 var QRCode;
 
 (function () {
